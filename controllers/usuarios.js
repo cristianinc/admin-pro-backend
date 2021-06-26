fc63cd7e4a0fcf2c42b25fc986da0a288595989c
Cristian Montecinos <cristian@pop-os.localdomain>
2021-06-26 00:04:23 -0400

    update de usuario

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,5 +1,5 @@
 const { response } = require('express');
-const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
 
@@ -18,17 +18,7 @@ const getUsuarios = async (req, res) => {
 
 const crearUsuario = async (req, res = response) => {
     
-    const { email, password, nombre } = req.body;
-
-    const errores = validationResult(req);
-
-    if( !errores.isEmpty() ){
-        return res.status(400).json({
-            ok: false,
-            errors: errores.mapped()
-        })
-    }
-
+    const { email, password } = req.body;
 
 
     try{
@@ -42,8 +32,13 @@ const crearUsuario = async (req, res = response) => {
             })
         }
 
-
         const usuario = new Usuario( req.body );
+        
+        //Encriptar contraseña
+        const salt = bcrypt.genSaltSync();
+        usuario.password = bcrypt.hashSync( password, salt );
+        
+        
         await usuario.save();
     
         res.json({
@@ -64,4 +59,57 @@ const crearUsuario = async (req, res = response) => {
 
 }
 
-module.exports = { getUsuarios,crearUsuario }
+
+const actualizarUsuario = async (req, res = response) => {
+
+    //TODO: validar token y comprobar si el usuario es correcto
+
+
+    const uid = req.params.id;
+
+    try {
+        
+        const usuarioDB = await Usuario.findById( uid );
+
+       
+
+        if( !usuarioDB ){
+            res.status(404).json({
+                ok: false,
+                msg: 'No existe un usuario por ese id'
+            });
+        }
+
+        //actualizar
+        const { password, google, email, ...campos } = req.body;
+        
+        if( usuarioDB.email != email ){
+            const existeEmail = await Usuario.findOne({ email });
+            if( existeEmail){
+                res.status(400).json({
+                    ok: false,
+                    msg: 'Ya existe un usuario con ese Email.'
+                });
+            }
+        }
+        campos.email = email;
+        const usuarioActualizado = await Usuario.findOneAndUpdate( uid, campos, { new:true } );
+
+
+        res.json({
+            ok: true,
+            usuario: usuarioActualizado
+        })
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error inesperado'
+        })
+    }
+
+}
+
+module.exports = { getUsuarios,crearUsuario, actualizarUsuario }
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -5,9 +5,9 @@
 
 const { Router } =  require('express');
 const { check } = require('express-validator')
+const { validarCampos } = require('../middlewares/validar-campos');
 
-
-const { getUsuarios,crearUsuario } = require('../controllers/usuarios')
+const { getUsuarios,crearUsuario, actualizarUsuario } = require('../controllers/usuarios')
 
 const router = Router();
 
@@ -18,6 +18,15 @@ router.post( '/', [
                         check('nombre', 'El nombre es obligatorio').not().isEmpty(),
                         check('password', 'El password es obligatorio').not().isEmpty(),
                         check('email', 'El email es obligatorio').isEmail(),
+                        validarCampos,
                     ] ,crearUsuario);
+
+
+router.put( '/:id', [
+                        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+                        check('email', 'El email es obligatorio').isEmail(),
+                        check('role', 'El role es obligatorio').not().isEmpty(),
+                        validarCampos,
+                    ], actualizarUsuario);
 
 module.exports = router;
